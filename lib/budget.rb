46e3c09c1b7542a1c6f228be8f71a8a887820b62
Elle Majors <hellolovely@ellemajors.com>
2023-01-13 11:00:45 -0700

    feat: #low_expenses can return list of departments with expenses less that 500

--- lib/budget.rb
+++ lib/budget.rb
@@ -10,9 +10,13 @@ class Budget
   def add_department(department)
     @departments << department
   end
+
+  def low_expenses
+    @departments.map do |department|
+      department if department.expenses < 500
+    end.compact
+  end
 end  
 
 
-
-# A budget can list all departments with expenses less than $500
 # A budget can list all employees' salaries
