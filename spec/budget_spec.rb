2283be8517cb3746b0a0b702e001df909012dabc
Elle Majors <hellolovely@ellemajors.com>
2023-01-13 10:59:34 -0700

    test: #low_expenses

--- spec/budget_spec.rb
+++ spec/budget_spec.rb
@@ -24,5 +24,24 @@ RSpec.describe 'Budget' do
 
       expect(budget.departments).to eq([customer_service, accounting, hr])
     end
+
+    it 'can return list of departments with expenses less than 500' do
+      budget = Budget.new("2023")
+      customer_service = Department.new("Customer Service")   
+      accounting = Department.new("Accounting")
+      hr = Department.new("HR")
+
+      budget.add_department(customer_service)
+      budget.add_department(accounting)
+      budget.add_department(hr)
+      
+      customer_service.expense(100)
+      customer_service.expense(25)
+      customer_service.expense(400)
+      accounting.expense(100)
+      hr.expense(50)
+
+      expect(budget.low_expenses).to eq([accounting, hr])
+    end
   end
 end
